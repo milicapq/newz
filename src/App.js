37fc47aade76a7066f7aeb0dce89e94a09dcdc6e
milicapq <117392234+milicapq@users.noreply.github.com>
2024-04-15 15:18:18 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <p>ashjaksn</p>
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default App;
