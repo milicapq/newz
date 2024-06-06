263b5690fde5f3a2fac182dbd679a2fca641fc59
Darko Stojanovic <info@itmedia.io>
2024-06-06 14:39:48 +0200

    news folder

---
+++ src/page/news/category.js
@@ -0,0 +1,17 @@
+import "./App.scss";
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+
+function App() {
+    return (
+        <div className="About">
+            <Header />
+            <div>
+                Under construction!
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
